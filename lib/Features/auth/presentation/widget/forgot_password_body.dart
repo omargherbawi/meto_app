149d58be54e41fe8826ab2ab49809bd881e4f690
Omar <omarahmad12318@gmail.com>
2025-08-29 16:22:02 +0300

    forgot password done

--- lib/Features/auth/presentation/widget/forgot_password_body.dart
+++ lib/Features/auth/presentation/widget/forgot_password_body.dart
@@ -1,10 +1,106 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:meto_application/config/app_colors.dart';
+import 'package:meto_application/core/validation/text_field_validation.dart';
 
-class ForgotPasswordBody extends StatelessWidget {
+class ForgotPasswordBody extends StatefulWidget {
   const ForgotPasswordBody({super.key});
 
   @override
+  State<ForgotPasswordBody> createState() => _ForgotPasswordBodyState();
+}
+
+class _ForgotPasswordBodyState extends State<ForgotPasswordBody> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  void _onSendResetLinkPressed() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Form is valid, proceed with sending reset link
+      print('Email: ${_emailController.text}');
+      // TODO: Implement actual password reset logic here
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Enter your email address'.tr(),
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _emailController,
+              validator: TextFieldValidation.validateEmail,
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: AppColors.primaryColor,
+              decoration: InputDecoration(
+                hintText: 'Example@gmail.com',
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.primaryColor.withOpacity(0.6),
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red, width: 2),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red, width: 2),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: _onSendResetLinkPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                'SendResetLink'.tr(),
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/core/widget/custom_app_bar.dart
@@ -0,0 +1,54 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:meto_application/config/app_colors.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.leading,
+    this.backgroundColor,
+    this.toolbarHeight,
+    this.systemUI,
+  });
+
+  final String? title;
+  final Widget? leading;
+  final Color? backgroundColor;
+  final double? toolbarHeight;
+  final SystemUiOverlayStyle? systemUI;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      scrolledUnderElevation: 0,
+      toolbarHeight: toolbarHeight,
+      systemOverlayStyle: systemUI ?? SystemUiOverlayStyle.light,
+      automaticallyImplyLeading: false,
+      leading:
+          leading ??
+          IconButton(
+            onPressed: () {
+              Get.back();
+            },
+            icon: Icon(Icons.arrow_back_ios_new, color: AppColors.white),
+          ),
+      backgroundColor: backgroundColor ?? AppColors.primaryColor,
+      centerTitle: true,
+      title: Text(
+        title!.tr(),
+        style: TextStyle(
+          color: AppColors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
